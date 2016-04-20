b9a9210967c0e932802b338629e74f3b14316605
allendecid <diesirae18@gmail.com>
2016-04-20 13:34:45 -0300

    Update AIchatload.js

--- EN/AIchatload.js
+++ EN/AIchatload.js
@@ -1,4 +1,20 @@
+/* Copyright ONL SpA 2016  Christian Allende
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Contact: christian.allende.cid@gmail.com
+    */
 function init(csv,decider,reg,lengthmax,memory,coef, weights){
 
 d3.csv(csv, function(error, data) {
